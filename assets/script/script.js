472fcfa2b7468114e54e8055a915f7895603d85b
Andros <aego.hgo@gmail.com>
2020-04-18 01:05:04 -0500

    Create script.js

---
+++ assets/script/script.js
@@ -0,0 +1,25 @@
+/*--=====================================
+				        Location
+======================================--*/
+if ("geolocation" in navigator) {
+  // console.log("Localización disponible");
+} else {
+  // console.log("Localización NO disponible");
+}
+var watchID = navigator.geolocation.watchPosition(function(position) {});
+navigator.geolocation.clearWatch(watchID);
+function geo_success(position) {
+  document.getElementById('geo').innerHTML=(position.coords.latitude + ' ' + position.coords.longitude);
+}
+function geo_error() {
+  // console.log("No se proporciono permiso.");
+}
+var geo_options = {
+  enableHighAccuracy: true, 
+  maximumAge        : 30000, 
+  timeout           : 27000
+};
+var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+function errorCallback(error) {
+  // console.log('ERROR(' + error.code + '): ' + error.message);
+};
